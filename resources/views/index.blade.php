3c76f410af07270127ce05106127b114cbafb00a
masakachristopher <masakachristopher@yahoo.com>
2023-04-01 00:44:51 +0300

    Modify file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,16 @@
 <head>
     <title>Import data from CSV file</title>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
 </head>
 
 <body>
+    <style>
+
+    </style>
 
     <div class="container mt-5">
 
@@ -23,14 +28,16 @@
             </div>
         @endif
 
-        <div class="card">
+        <div>
 
-            <div class="card-header font-weight-bold">
-                <h2 class="float-right"><a href="{{ url('export/xlsx') }}"
-                        class="btn btn-success mr-1">Export Excel</a>
+            <div class="font-weight-bold">
+                <h6 class="float-right">
+                    <a href="{{ url('export/xlsx') }}" class="mr-4"><span> <i>Excel export</i></span></a>
+                    <a href="{{ url('diamonds') }}" class="mr-1"><span> <i>Show diamonds data</i></span></a>
+                </h6>
             </div>
 
-            <div class="card-body">
+            <div>
 
                 <form id="excel-csv-import-form" method="POST" action="{{ url('import') }}"
                     accept-charset="utf-8" enctype="multipart/form-data">
